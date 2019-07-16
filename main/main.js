656c494ac6e942a894d1e248a2de0db54b23e8be
CyrilYao <cyril.yao@oocl.com>
2019-07-16 09:50:14 +0800

    refactor: naming of the variable and the used of map, distinguish the different of "===" and "=="

--- main/main.js
+++ main/main.js
@@ -1,18 +1,17 @@
 'use strict';
 
 function printReceipt(inputs){
-    var itemObject = countNumOfEachItem(inputs);
+    var itemAndNum = countNumOfEachItem(inputs);
     var total=0.00;
     var discountedMoney=0;
     var result = '***<没钱赚商店>收据***\n'
-    for(var barcode in itemObject){
+    for(var barcode in itemAndNum){
         let item = getItemByBarcode(barcode);
-        let num = itemObject[barcode];
+        let num = itemAndNum[barcode];
         let subTotal = getSubTotal(item, num);
         discountedMoney += subTotal;
         total += Number(item.price * num);
         result += `名称：${item.name}，数量：${num}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${subTotal.toFixed(2)}(元)\n`;
-        // '名称：'+ item.name +'，数量：'+ num + item.unit + '，单价：'+ item.price.toFixed(2) +'(元)，小计：'+ (item.price * num).toFixed(2) +'(元)';
     }
     result+=`----------------------\n总计：${discountedMoney.toFixed(2)}(元)\n节省：${(total - discountedMoney).toFixed(2)}(元)\n**********************`;
 
@@ -20,32 +19,31 @@ function printReceipt(inputs){
 }
 
 function countNumOfEachItem(inputs){
-    var itemObject = {};
-    // TODO 可以改成用reduce实现
-    inputs.map(tag => {
+    var itemAndNum = {};
+    inputs.forEach(tag => {
         let splitTag = tag.split('-');
         if(splitTag.length===1){
-            if(itemObject[tag]){
-                itemObject[tag]++;
+            if(itemAndNum[tag]){
+                itemAndNum[tag]++;
             } else {
-                itemObject[tag]=1;
+                itemAndNum[tag]=1;
             }
         } else {
             let barcode = splitTag[0];
             let num = Number(splitTag[1]);
-            if(itemObject[barcode]){
-                itemObject[barcode]+=num;
+            if(itemAndNum[barcode]){
+                itemAndNum[barcode]+=num;
             } else {
-                itemObject[barcode]=num;
+                itemAndNum[barcode]=num;
             }
         }
     });
-    return itemObject;
+    return itemAndNum;
 }
 
 function getItemByBarcode(barcode){
     var allItems = loadAllItems();
-    return allItems.find(item => item.barcode == barcode);
+    return allItems.find(item => item.barcode === barcode);
 }
 
 function getSubTotal(item, num){
